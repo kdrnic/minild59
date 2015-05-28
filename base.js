0bc0dd687144d9b33079bc33dce46b819408efb3
lucasdealmeidasm <lucasdealmeidasm@gmail.com>
2015-05-28 16:10:59 -0300

    Aadasdadasdas

--- base.js
+++ base.js
@@ -1,6 +1,7 @@
-var pressed = [];
-var preventDefault = false;
 var mouse = {};
+var pressed = [];
+var entityConstructors = {};
+var entities = [];
 
 function MouseUpEvent(event){
 	mouse.pressed[event.button] = false;
@@ -56,11 +57,39 @@ function Start(){
 	var t = new Image();
 	t.onload = function(){ 
 		tileset = BreakImage(this, 16, 16);
-		window.requestAnimationFrame(DoFrame);
+		LoadTextFile("level.json", function(m){
+			InitMap(JSON.parse(m));
+			window.requestAnimationFrame(DoFrame);
+		});
 	}
 	t.src = "tileset.png";
 	
 	frame = 0;
+}
+
+function InitMap(jsonObj){
+	var tileLayer = null, objLayer = null;
+	for(var i = 0; i < jsonObj.layers.length; i++){
+		if(jsonObj.layers[i].type == "tilelayer") tileLayer = jsonObj.layers[i];
+		if(jsonObj.layers[i].type == "objectgroup") objLayer = jsonObj.layers[i];
+	}
+	if(tileLayer){
+		map = [];
+		for(var x = 0; x < tileLayer.width; x++){
+			map[x] = [];
+			for(var y = 0; y < tileLayer.height; y++){
+				map[x][y] = tileLayer.data[x + y * tileLayer.width] - 1;
+			}
+		}
+	}
+	if(objLayer){
+		for(var i = 0; i < objLayer.objects.length; i++){
+			if(!entityConstructors.hasOwnProperty(objLayer.objects[i].type)) continue;
+			var e = new entityConstructors[objLayer.objects[i].type]();
+			e.Init(objLayer.objects[i]);
+			 entities.push(e);
+		}
+	}
 }
 
 function DoFrame(){
@@ -72,6 +101,73 @@ function DoFrame(){
 
 function Update(){
 	UpdateKeys();
+	UpdateEntities();
+}
+
+function UpdateEntities(){
+	for(var i = 0; i < entities.length; i++){
+		if(typeof entities[i].Update == "function") entities[i].Update();
+	}
+	MoveEntities();
+}
+
+function Entity(){
+}
+
+Entity.prototype.Init = function(obj){
+	this.x = obj.x << 8;
+	this.y = obj.y << 8;
+}
+
+Entity.prototype.MapCollision = function(){
+	if(!this.solid) return false;
+	for(var x = this.x >> 12; x <= (this.x + 0xf00) >> 12; x++){
+		if(x < 0) continue;
+		if(x >= map.length) continue;
+		for(var y = this.y >> 12; y <= (this.y + 0xf00) >> 12; y++){
+			if(y < 0) continue;
+			if(y >= map[x].length) continue;
+			if((map[x][y] >= 32) && (map[x][y] < 48)) return true;
+		}
+	}
+	return false;
+}
+
+function MoveEntities(){
+	for(var i = 0; i < entities.length; i++){
+		var e = entities[i];
+		if(e.dx){
+			e.x += e.dx;
+			if(e.MapCollision()){
+				if(e.dx > 0) e.x -= e.x & 0xfff;
+				if(e.dx < 0) e.x += ((~(e.x & 0xfff)) >>> 0);
+			}
+		}
+		if(e.dy){
+			e.y += e.dy;
+			if(e.MapCollision()){
+				if(e.dy > 0) e.y -= e.y & 0xfff;
+				if(e.dy < 0) e.y += ((~(e.y & 0xfff)) >>> 0);
+			}
+		}
+	}
+}
+
+function Draw(){
+	context.fillRect(0, 0, 256, 240);
+	context.save();
+	context.translate(-scrollX, -scrollY);
+	MapDraw();
+	DrawEntities();
+	context.restore();
+}
+
+function DrawEntities(){
+	for(var i = 0; i < entities.length; i++){
+		if(entities[i].hasOwnProperty("sprTile")){
+			context.drawImage(tileset[entities[i].sprTile], entities[i].x >> 8, entities[i].y >> 8);
+		}
+	}
 }
 
 function Draw(){
@@ -101,9 +197,9 @@ function BreakImage(img, w, h){
 
 function MapDraw(){
 	if(!window.hasOwnProperty("map")) return;
-	for(var i = scrollX >> 4; i < Math.min(1 + (scrollX + canvas.width) >> 4, map.length); i++){
-		for(var j = scrollY >> 4; j < Math.min(1 + (scrollY + canvas.height) >> 4, map[i].length); j++){
-			context.drawImage(tileset[map[i][j]], i * 16, j * 16);
+	for(var i = Math.max(Math.floor(scrollX / 16), 0); i < Math.min(1 + Math.floor((scrollX + canvas.width) / 16), map.length); i++){
+		for(var j = Math.max(Math.floor(scrollY / 16), 0); j < Math.min(1 + Math.floor((scrollY + canvas.height) / 16), map[i].length); j++){
+			if(map[i][j] >= 0) context.drawImage(tileset[map[i][j]], i * 16, j * 16);
 		}
 	}
 }
@@ -114,3 +210,31 @@ function CanvasZoom(factor){
 		canvas.style.height = canvas.style.height.split("px")[0] * factor;
 	}
 }
+
+function Player(obj){
+	this.sprTile = 0;
+	this.solid = true;
+}
+
+Player.prototype = new Entity;
+
+Player.prototype.Update = function(){
+	if(keys.keyRight.state){
+		this.dx += 0xf0;
+	}
+	else if(keys.keyLeft.state){
+		this.dx -= 0xf0;
+	}
+	else this.dx = this.dx >> 2;
+	if((keys.keyUp.state == 1) && (true)){
+		this.dy = -0x500;
+	}
+	else this.dy += 0x80;
+	if(this.dx < -0x300) this.dx = -0x300;
+	if(this.dx > 0x300) this.dx = 0x300;
+	if(this.dy > 0x500) this.dy = 0x500;
+	scrollX = (this.x >> 8) + 8 - 128;
+	scrollY = (this.y >> 8) + 8 - 128;
+}
+
+entityConstructors["player"] = Player;
